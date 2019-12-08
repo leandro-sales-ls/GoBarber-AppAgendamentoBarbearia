82daa0f75f41b05efc4617ca144ad803de65a2bf
leandro <leandro.sallesls@hotmail.com>
2019-12-08 15:09:12 -0300

    Primeiro commit

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dialect: "postgres",
+  host: "127.0.0.1",
+  port: "5432",
+  username: "postgres",
+  password: "13303230",
+  database: "gonodemodulo2",
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
